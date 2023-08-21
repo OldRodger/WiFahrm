668a2f8d1210a7a1e7caf0262dc22d192d44f37a
Celestine Nwachukwu <celexspencer007@gmail.com>
2023-08-21 23:40:37 +0100

    created navigation context file

--- src/components/navigation/mobile-navigation.tsx
+++ src/components/navigation/mobile-navigation.tsx
@@ -1,6 +1,8 @@
+import { Bars3BottomRightIcon } from "@heroicons/react/24/solid";
+
 function MobileNav() {
-    return <div>
-        
+    return <div className="fixed top-5 right-9 inline-block p-3 rounded-full bg-primary-500" >
+        <Bars3BottomRightIcon className="w-8 h-8 text-white" />
     </div>;
 }
 
