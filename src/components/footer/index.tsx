168a41bd17e3cbfbc42a792e1ec4a1b880241771
Celestine Nwachukwu <celexspencer007@gmail.com>
2023-08-22 21:21:21 +0100

    fixed minor color difference on mobile

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return <footer>
         <div className="bg-footer-wave bg-cover bg-center py-20 md:py-36">
             <div className="w-5/6 mx-auto">
-                <div className="bg-white text-center md:rounded-md md:py-20 md:shadow-2xl">
+                <div className=" text-center md:rounded-md md:bg-white md:py-20 md:shadow-2xl">
                     <h1 className="text-xl tracking-wider font-semibold mb-8 md:text-2xl">The future of <span className="text-primary-300 capitalize">farm investing</span> is Wifahrm</h1>
                     <button className="capitalize px-12 py-2 bg-primary-300 text-white rounded-md">invest now</button>
                 </div>
--- src/pages/home/hero.tsx
+++ src/pages/home/hero.tsx
@@ -2,7 +2,7 @@ import { Image7 } from "@/assets";
 
 function Hero() {
     return <header className=" py-10 md:bg-hero-background bg-center bg-cover h-full">
-        <div className="w-5/6 mx-auto h-full items-center md:h-5/6 md:flex">
+        <div className="w-5/6 border-b-2 mx-auto h-full items-center md:h-5/6 md:flex md:border-none">
             {/* MAIN HEADER */}
             <div className="md:basis-3/5 mt-20 md:mt-0 flex items-center">
                 <div className="sm:w-3/5 ">
